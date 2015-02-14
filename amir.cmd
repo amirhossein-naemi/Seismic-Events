6facdc5f291ef53edd9ef1b40316796a90fd3846
amirhossein-naemi <amirhnaimi@gmail.com>
2015-02-14 02:19:38 -0600

    Delete amir.cmd

--- amir.cmd
+++
@@ -1,9 +0,0 @@
-rem gcc -o hellomake hellomake.c hellofunc.c -I.
-rem The -I. is included so that gcc will look in the current directory (.) for the include file hellomake.h.
-rem gcc -o amir.exe amir.cpp
-rem g++ -o amir.exe amir.cpp
-rem make amir.mk
-make -f amir.mk
-make -f amir.mk clean
-rem The -I. is included so that gcc will look in the current directory (.) for the include file hellomake.h.
-pause
